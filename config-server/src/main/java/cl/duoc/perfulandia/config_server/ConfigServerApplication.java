0f43172203f109f4f04848acb231d42cfb2f5634
da.zapatal@duocuc.cl <da.zapatal@duocuc.cl>
2026-01-16 03:03:38 -0300

    se actualizo el config-server

--- config-server/src/main/java/cl/duoc/perfulandia/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/cl/duoc/perfulandia/config_server/ConfigServerApplication.java
@@ -2,12 +2,12 @@ package cl.duoc.perfulandia.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer; // Asegúrate de que esta línea esté
 
 @SpringBootApplication
+@EnableConfigServer // <--- ESTA ES LA CLAVE
 public class ConfigServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
