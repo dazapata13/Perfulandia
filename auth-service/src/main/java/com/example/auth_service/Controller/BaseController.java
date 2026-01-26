f4117631fad19ba4e081c9db4de304093dcf21f5
dazapata13 <da.zapatal@duocuc.cl>
2026-01-26 03:39:44 -0300

    asd

--- auth-service/src/main/java/com/example/auth_service/Controller/BaseController.java
+++ auth-service/src/main/java/com/example/auth_service/Controller/BaseController.java
@@ -1,10 +1,31 @@
-package com.example.auth_service.Controller; 
+package com.example.auth_service.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class BaseController {
+@RestController
+@RequestMapping("/")
+public class BaseController {
+
+    @Value("${app.nombre}")
+    private String nombreApp;
+
+    @Value("${app.version}")
+    private String version;
+
+    @GetMapping("")
+    public Map<String, String> base() {
+        Map<String, String> info = new HashMap<>();
+        info.put("nombre", nombreApp);
+        info.put("version", version);
+        return info;
+    }
+
     protected ResponseEntity<Map<String, Object>> successResponse(Object data, String message) {
         Map<String, Object> response = new HashMap<>();
         response.put("success", true);
